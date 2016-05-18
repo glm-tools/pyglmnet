c8b4cb7a94c1208d7e1d1cd03a28659c1b6f8b3a
tallesventura <toliveiradesouzaalv@luc.edu>
2016-05-18 13:42:22 -0500

    refactored GLM variable names in the test file so that same objects have same names

--- tests/test_pyglmnet.py
+++ tests/test_pyglmnet.py
@@ -58,12 +58,12 @@ def test_glmnet():
     glm.deviance(y_train, y_pred)
 
     # don't allow slicing if model has not been fit yet.
-    glm = GLM(distr='poisson')
-    assert_raises(ValueError, glm.__getitem__, 2)
+    glm_poisson = GLM(distr='poisson')
+    assert_raises(ValueError, glm_poisson.__getitem__, 2)
 
     # test fit_predict
-    glm.fit_predict(X_train, y_train)
-    assert_raises(ValueError, glm.fit_predict, X_train[None, ...], y_train)
+    glm_poisson.fit_predict(X_train, y_train)
+    assert_raises(ValueError, glm_poisson.fit_predict, X_train[None, ...], y_train)
 
 
 def simple_cv_scorer(obj, X, y):
@@ -76,46 +76,46 @@ def simple_cv_scorer(obj, X, y):
 def test_cv():
     """Simple CV check"""
     X, y = make_regression()
-    model_mn = GLM(distr='normal', alpha=0.01, reg_lambda=np.array([0.0, 0.1, 0.2]))
-    model_mn.fit(X, y)
+    glm_normal = GLM(distr='normal', alpha=0.01, reg_lambda=np.array([0.0, 0.1, 0.2]))
+    glm_normal.fit(X, y)
 
     cv = KFold(X.shape[0], 5)
 
     # check that it returns 5 scores
-    assert_equal(len(cross_val_score(model_mn, X, y, cv=cv, scoring=simple_cv_scorer)), 5)
+    assert_equal(len(cross_val_score(glm_normal, X, y, cv=cv, scoring=simple_cv_scorer)), 5)
 
 
 def test_multinomial():
     """Test all multinomial functionality"""
-    glm = GLM(distr='multinomial', reg_lambda=np.array([0.0, 0.1, 0.2]), tol=1e-10)
+    glm_mn = GLM(distr='multinomial', reg_lambda=np.array([0.0, 0.1, 0.2]), tol=1e-10)
     X = np.array([[-1, -2, -3], [4, 5, 6]])
     y = np.array([1, 0])
     # test gradient
     beta = np.zeros([4, 2])
-    grad_beta0, grad_beta = glm.grad_L2loss(beta[0], beta[1:], 0, X, y)
+    grad_beta0, grad_beta = glm_mn.grad_L2loss(beta[0], beta[1:], 0, X, y)
     assert grad_beta0[0] != grad_beta0[1]
-    glm.fit(X, y)
-    y_pred = glm.predict(X)
+    glm_mn.fit(X, y)
+    y_pred = glm_mn.predict(X)
     assert_equal(y_pred.shape, (3, X.shape[0], 2))  # n_lambdas x n_samples x n_classes
     # pick one as yhat
     yhat = y_pred[0]
     # uniform prediction
     ynull = np.ones(yhat.shape) / yhat.shape[1]
     # pseudo_R2 should be greater than 0
-    assert_true(glm.pseudo_R2(y, yhat, ynull) > 0.)
-    glm.deviance(y, yhat)
-    assert_equal(len(glm.simulate(glm.fit_[0]['beta0'],
-                                  glm.fit_[0]['beta'],
+    assert_true(glm_mn.pseudo_R2(y, yhat, ynull) > 0.)
+    glm_mn.deviance(y, yhat)
+    assert_equal(len(glm_mn.simulate(glm_mn.fit_[0]['beta0'],
+                                  glm_mn.fit_[0]['beta'],
                                   X)),
                  X.shape[0])
     # these should raise an exception
     try:
-        glm.pseudo_R2(y, y, y)
+        glm_mn.pseudo_R2(y, y, y)
         assert False
     except Exception:
         assert True
     try:
-        glm.deviance(y, y)
+        glm_mn.deviance(y, y)
         assert False
     except Exception:
         assert True
