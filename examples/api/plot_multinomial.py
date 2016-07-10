9847b17668edb5d052e7b63bc7426236086d6ca8
pavanramkumar <pavan.ramkumar@gmail.com>
2016-07-10 00:00:59 -0500

    fixed bugs in multinomial example

--- examples/api/plot_multinomial.py
+++ examples/api/plot_multinomial.py
@@ -3,11 +3,8 @@
 Multinomial Distribution
 =========================
 
-This is an example demonstrating `'Pyglmnet'` with
+This is an example demonstrating Pyglmnet with
 multinomial distributed targets, typical in classification problems.
-
-Here, we deal with the numerical instability of the exponential
-link function by linearizing it above a certain threshold, ``eta``.
 
 This canonical link can be used by specifying ``distr`` = ``'multinomial'``.
 
@@ -15,8 +12,8 @@ This canonical link can be used by specifying ``distr`` = ``'multinomial'``.
 ##########################################################
 # Multinomial example
 # ^^^^^^^^^^^^^^^^^^^
-# We can also use ``pyglmnet`` with multinomial case
-# where you can provide an array of class labels as targets.
+# In the multinomial case
+# you can provide an array of class labels as targets.
 
 ##########################################################
 
@@ -31,11 +28,19 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 ########################################################
+# Setup the model using scikit learn convenience functions
+########################################################
 
+########################################################
 from sklearn.datasets import make_classification
 X, y = make_classification(n_samples=10000, n_classes=5,
                            n_informative=100, n_features=100, n_redundant=0)
 
+########################################################
+# Fit the model
+########################################################
+
+########################################################
 # import GLM model
 from pyglmnet import GLM
 glm_mn = GLM(distr='multinomial', alpha=0.01,
@@ -43,4 +48,4 @@ glm_mn = GLM(distr='multinomial', alpha=0.01,
 glm_mn.threshold = 1e-5
 glm_mn.fit(X, y)
 y_pred = glm_mn[-1].predict(X).argmax(axis=1)
-print('Output performance = %f percent.' % (y_pred == y).mean())
+print('Percentage correct = %f percent.' % (y_pred == y).mean())
