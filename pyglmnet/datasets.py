ee005ca13cc15255fcaceff65ac974a9fef727d4
pavanramkumar <pavan.ramkumar@gmail.com>
2016-10-27 19:33:48 -0500

    flake8 fixes and coveragerc edits

--- pyglmnet/datasets.py
+++ pyglmnet/datasets.py
@@ -4,26 +4,30 @@ A set of convenience functions to download datasets for illustrative examples
 import subprocess
 import pandas as pd
 
+
 def fetch_tikhonov_data(dpath='/tmp/glm-tools'):
     cmd = 'rm -rf ' + dpath
     subprocess.call(cmd, shell=True)
     cmd = 'mkdir ' + dpath
     subprocess.call(cmd, shell=True)
 
-    fname = dpath+'/' + 'fixations.csv'
-    url = 'https://raw.githubusercontent.com/glm-tools/datasets/master/tikhonov/fixations.csv'
+    fname = dpath + '/' + 'fixations.csv'
+    url = ("https://raw.githubusercontent.com/"
+           "glm-tools/datasets/master/tikhonov/fixations.csv")
     cmd = 'curl -o ' + fname + ' ' + url
     subprocess.call(cmd, shell=True)
     fixations_df = pd.read_csv(fname)
 
-    fname = dpath+'/' + 'probes.csv'
-    url = 'https://raw.githubusercontent.com/glm-tools/datasets/master/tikhonov/probes.csv'
+    fname = dpath + '/' + 'probes.csv'
+    url = ("https://raw.githubusercontent.com/"
+           "glm-tools/datasets/master/tikhonov/probes.csv")
     cmd = 'curl -o ' + fname + ' ' + url
     subprocess.call(cmd, shell=True)
     probes_df = pd.read_csv(fname)
 
-    fname = dpath+'/' + 'spiketimes.csv'
-    url = 'https://raw.githubusercontent.com/glm-tools/datasets/master/tikhonov/spiketimes.csv'
+    fname = dpath + '/' + 'spiketimes.csv'
+    url = ("https://raw.githubusercontent.com/"
+           "glm-tools/datasets/master/tikhonov/spiketimes.csv")
     cmd = 'curl -o ' + fname + ' ' + url
     subprocess.call(cmd, shell=True)
     spikes_df = pd.read_csv(fname, header=None)
