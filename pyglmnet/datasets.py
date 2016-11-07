396ce93cbd6bb6af2eae4f466b6ebf416e40ec4f
pavanramkumar <pavan.ramkumar@gmail.com>
2016-11-07 13:37:59 -0600

    added urllib and os.path.join for os independence

--- pyglmnet/datasets.py
+++ pyglmnet/datasets.py
@@ -2,7 +2,9 @@
 A set of convenience functions to download datasets for illustrative examples
 """
 import subprocess
+import urllib
 import pandas as pd
+import os
 
 
 def fetch_tikhonov_data(dpath='/tmp/glm-tools'):
@@ -11,25 +13,22 @@ def fetch_tikhonov_data(dpath='/tmp/glm-tools'):
     cmd = 'mkdir ' + dpath
     subprocess.call(cmd, shell=True)
 
-    fname = dpath + '/' + 'fixations.csv'
+    fname = os.path.join(dpath, 'fixations.csv')
     url = ("https://raw.githubusercontent.com/"
            "glm-tools/datasets/master/tikhonov/fixations.csv")
-    cmd = 'curl -o ' + fname + ' ' + url
-    subprocess.call(cmd, shell=True)
+    urllib.urlretrieve(url, fname)
     fixations_df = pd.read_csv(fname)
 
-    fname = dpath + '/' + 'probes.csv'
+    fname = os.path.join(dpath, 'probes.csv')
     url = ("https://raw.githubusercontent.com/"
            "glm-tools/datasets/master/tikhonov/probes.csv")
-    cmd = 'curl -o ' + fname + ' ' + url
-    subprocess.call(cmd, shell=True)
+    urllib.urlretrieve(url, fname)
     probes_df = pd.read_csv(fname)
 
-    fname = dpath + '/' + 'spiketimes.csv'
+    fname = os.path.join(dpath, 'spiketimes.csv')
     url = ("https://raw.githubusercontent.com/"
            "glm-tools/datasets/master/tikhonov/spiketimes.csv")
-    cmd = 'curl -o ' + fname + ' ' + url
-    subprocess.call(cmd, shell=True)
+    urllib.urlretrieve(url, fname)
     spikes_df = pd.read_csv(fname, header=None)
 
     return fixations_df, probes_df, spikes_df
