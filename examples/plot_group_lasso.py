c102f174bf3d87e56ae7f688ea4dcc256ec26526
pavanramkumar <pavan.ramkumar@gmail.com>
2016-11-16 17:22:43 -0600

    made examples flow better and removed poisson/softplus demos

--- examples/plot_group_lasso.py
+++ examples/plot_group_lasso.py
@@ -1,56 +1,65 @@
 """
-=========================
-Group Lasso Example
-=========================
+===========================
+Group Lasso Regularization
+===========================
 
-This is an example demonstrating Pyglmnet with
-multinomial the group lasso regularization, typical in regression
-problems where it is reasonable to impose penalties to model parameters
-in a group-wise fashion based on domain knowledge.
-
+This is an example demonstrating Pyglmnet with group lasso regularization,
+typical in regression problems where it is reasonable to impose penalties
+to model parameters in a group-wise fashion based on domain knowledge.
 
 """
+########################################################
+
+# Author: Matthew Antalek <matthew.antalek@northwestern.edu>
+# License: MIT
+
+########################################################
 
 from pyglmnet import GLM
 from pyglmnet.datasets import fetch_group_lasso_datasets
 import numpy as np
-import random
+import matplotlib.pyplot as plt
 
 ##########################################################
 #
 #Group Lasso Example
-#similar to method found in:
+#applied to the same dataset found in:
 #ftp://ftp.stat.math.ethz.ch/Manuscripts/buhlmann/lukas-sara-peter.pdf
 #
 #The task here is to determine which base pairs and positions within a 7-mer
-#sequence are most important to predicting if the sequence contains a splice
+#sequence are predictive of whether the sequence contains a splice
 #site or not.
 #
-##########################################################
 
-print("Retrieving data...")
+##########################################################
+# Read and preprocess data
 
 df , group_idxs= fetch_group_lasso_datasets()
-
-
-print("Data retrieved")
-print("Dataframe: ")
 print(df.head())
 
-#set up the group lasso GLM model
+##########################################################
+# Set up the training and testing sets
 
+X = df[df.columns.difference(["Label"])].values
+y = df.loc[:, "Label"].values
+
+from sklearn.cross_validation import train_test_split
+Xtrain, Xtest, ytrain, ytest = \
+    train_test_split(X, y, test_size=0.2, random_state=42)
+
+##########################################################
+# Setup the models
+
+#set up the group lasso GLM model
 gl_glm = GLM(distr="binomial",
              group=group_idxs,
-             tol=1e-2,
              score_metric="pseudo_R2",
              alpha=1.0,
              reg_lambda=np.logspace(np.log(100), np.log(0.01), 5, base=np.exp(1)))
 
 
-#set up the non group GLM model
-
+#set up the lasso model
 glm = GLM(distr="binomial",
-          tol=1e-2,
           score_metric="pseudo_R2",
           alpha=1.0,
           reg_lambda=np.logspace(np.log(100), np.log(0.01), 5, base=np.exp(1)))
@@ -58,26 +67,31 @@ glm = GLM(distr="binomial",
 print("gl_glm: ", gl_glm)
 print("glm: ", glm)
 
-# Set up the training and testing sets.
-X = df[df.columns.difference(["Label"]).values]
+##########################################################
+# Fit models
 
-test_idxs = random.sample(list(range(X.shape[0])), 1000)
-train_idxs = list( set(list(range(X.shape[0]))).difference(set(test_idxs)) )
+gl_glm.fit(Xtrain, ytrain)
+glm.fit(Xtrain, ytrain)
 
-X_train = X.iloc[train_idxs, :]
-X_test = X.iloc[test_idxs, :]
+##########################################################
+# Visualize model scores on test set
 
-y = df.loc[:, "Label"]
-y_train = y.iloc[train_idxs]
-y_test = y.iloc[test_idxs]
+plt.figure()
+plt.semilogx(gl_glm.reg_lambda, gl_glm.score(Xtest, ytest), 'go-')
+plt.semilogx(gl_glm.reg_lambda, gl_glm.score(Xtrain, ytrain), 'go--')
+plt.semilogx(glm.reg_lambda, glm.score(Xtest, ytest), 'ro-')
+plt.semilogx(glm.reg_lambda, glm.score(Xtrain, ytrain), 'ro--')
+plt.legend(['Group Lasso: test',
+            'Group Lasso: train',
+            'Lasso: test',
+            'Lasso: train'], frameon=False, loc='best')
+plt.xlabel('$\lambda$')
+plt.ylabel('pseudo-$R^2$')
+plt.ylim([-0.1, 0.7])
 
-
-print("Fitting models")
-gl_glm.fit(X_train.values, y_train.values)
-glm.fit(X_train.values, y_train.values)
-print("Model fitting complete.")
-print("\n\n")
-
-
-print("Group lasso post fitting score: ", gl_glm.score(X_test.values, y_test.values))
-print("Non-group lasso post fitting score: ", glm.score(X_test.values, y_test.values))
+plt.tick_params(axis='y', right='off')
+plt.tick_params(axis='x', top='off')
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.show()
