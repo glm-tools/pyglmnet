e672433538f802a4f89dc23a81c0537f31783ac2
Ravi Prakash Garg <ravigarg27@gmail.com>
2017-03-30 09:40:35 -0600

    Fixing flake8 errors

--- pyglmnet/metrics.py
+++ pyglmnet/metrics.py
@@ -1,62 +1,66 @@
+"""
+A set of scoring functions
+"""
 from . import utils
 import numpy as np
 
+
 def deviance(y, yhat, ynull_, distr):
 
-	if distr in ['softplus', 'poisson']:
-		LS = utils.log_likelihood(y, y, distr)
-	else:
-		LS = 0
+    if distr in ['softplus', 'poisson']:
+        LS = utils.log_likelihood(y, y, distr)
+    else:
+        LS = 0
 
-	scores = list()
-	
-	for idx in range(yhat.shape[0]):
-		if distr != 'multinomial':
-			yhat_this = (yhat[idx, :]).ravel()
-		else :
-			yhat_this = yhat[idx, :, :]
-		
-		L1 = utils.log_likelihood(y, yhat_this, distr)
-		scores.append(-2 * (L1 - LS))
-	
+    scores = list()
 
-	return np.array(scores)
+    for idx in range(yhat.shape[0]):
+        if distr != 'multinomial':
+            yhat_this = (yhat[idx, :]).ravel()
+        else:
+            yhat_this = yhat[idx, :, :]
+
+        L1 = utils.log_likelihood(y, yhat_this, distr)
+        scores.append(-2 * (L1 - LS))
+
+    return np.array(scores)
+
 
 def pseudo_R2(X, y, yhat, ynull_, distr):
 
-	if distr in ['softplus', 'poisson']:
-		LS = utils.log_likelihood(y, y, distr)
-	else:
-		LS = 0
-    
-	if distr != 'multinomial':
-		L0 = utils.log_likelihood(y, ynull_, distr)
-	else:
-		expand_ynull_ = np.tile(ynull_, (X.shape[0], 1))
-		L0 = utils.log_likelihood(y, expand_ynull_, distr)
-	
-	scores = list()
-	for idx in range(yhat.shape[0]):
-		
-		if distr != 'multinomial':
-			yhat_this = (yhat[idx, :]).ravel()
-		else:
-			yhat_this = yhat[idx, :, :]
+    if distr in ['softplus', 'poisson']:
+        LS = utils.log_likelihood(y, y, distr)
+    else:
+        LS = 0
 
-		L1 = utils.log_likelihood(y, yhat_this, distr)
-		        
-		if distr in ['softplus', 'poisson']:
-			scores.append(1 - (LS - L1) / (LS - L0))
-		else:
-			scores.append(1 - L1 / L0)
-	
-	return np.array(scores)
+    if distr != 'multinomial':
+        L0 = utils.log_likelihood(y, ynull_, distr)
+    else:
+        expand_ynull_ = np.tile(ynull_, (X.shape[0], 1))
+        L0 = utils.log_likelihood(y, expand_ynull_, distr)
+
+    scores = list()
+    for idx in range(yhat.shape[0]):
+
+        if distr != 'multinomial':
+            yhat_this = (yhat[idx, :]).ravel()
+        else:
+            yhat_this = yhat[idx, :, :]
+
+        L1 = utils.log_likelihood(y, yhat_this, distr)
+
+        if distr in ['softplus', 'poisson']:
+            scores.append(1 - (LS - L1) / (LS - L0))
+        else:
+            scores.append(1 - L1 / L0)
+
+    return np.array(scores)
+
 
 def accuracy(y, yhat):
 
-	scores = list()
-	for idx in range(yhat.shape[0]):
-		accuracy = float(np.sum(y == yhat[idx])) / len(yhat[idx])
-		scores.append(accuracy)
-	return np.array(scores)
-
+    scores = list()
+    for idx in range(yhat.shape[0]):
+        accuracy = float(np.sum(y == yhat[idx])) / len(yhat[idx])
+        scores.append(accuracy)
+    return np.array(scores)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,52 +1,55 @@
 import numpy as np
 from pyglmnet import GLM
-from nose.tools import assert_true, assert_equal, assert_raises
+from nose.tools import assert_equal
+
 
 def test_deviance():
-	n_samples, n_features = 1000, 100
+    n_samples, n_features = 1000, 100
 
-	beta0 = np.random.normal(0.0, 1.0, 1)
-	beta = np.random.normal(0.0, 1.0, n_features)
+    beta0 = np.random.normal(0.0, 1.0, 1)
+    beta = np.random.normal(0.0, 1.0, n_features)
 
     # sample train and test data
-	glm_sim = GLM(score_metric='deviance')
-	X = np.random.randn(n_samples, n_features)
-	y = glm_sim.simulate(beta0, beta, X)
+    glm_sim = GLM(score_metric='deviance')
+    X = np.random.randn(n_samples, n_features)
+    y = glm_sim.simulate(beta0, beta, X)
 
-	glm_sim.fit(X, y)
-	score = glm_sim[-1].score(X, y)
-	
-	assert_equal(score.shape[0], 1)
+    glm_sim.fit(X, y)
+    score = glm_sim[-1].score(X, y)
+
+    assert_equal(score.shape[0], 1)
+
 
 def test_pseudoR2():
-	n_samples, n_features = 1000, 100
+    n_samples, n_features = 1000, 100
 
-	beta0 = np.random.normal(0.0, 1.0, 1)
-	beta = np.random.normal(0.0, 1.0, n_features)
+    beta0 = np.random.normal(0.0, 1.0, 1)
+    beta = np.random.normal(0.0, 1.0, n_features)
 
     # sample train and test data
-	glm_sim = GLM(score_metric='pseudo_R2')
-	X = np.random.randn(n_samples, n_features)
-	y = glm_sim.simulate(beta0, beta, X)
+    glm_sim = GLM(score_metric='pseudo_R2')
+    X = np.random.randn(n_samples, n_features)
+    y = glm_sim.simulate(beta0, beta, X)
 
-	glm_sim.fit(X, y)
-	score = glm_sim[-1].score(X, y)
-	
-	assert_equal(score.shape[0], 1)
+    glm_sim.fit(X, y)
+    score = glm_sim[-1].score(X, y)
+
+    assert_equal(score.shape[0], 1)
+
 
 def test_accuracy():
 
-	n_samples, n_features, n_classes = 1000, 100, 2
+    n_samples, n_features, n_classes = 1000, 100, 2
 
-	beta0 = np.random.normal(0.0, 1.0, 1)
-	beta = np.random.normal(0.0, 1.0, (n_features, n_classes))
+    beta0 = np.random.normal(0.0, 1.0, 1)
+    beta = np.random.normal(0.0, 1.0, (n_features, n_classes))
 
     # sample train and test data
-	glm_sim = GLM(distr='binomial', score_metric='accuracy')
-	X = np.random.randn(n_samples, n_features)
-	y = glm_sim.simulate(beta0, beta, X)
-	y = np.argmax(y, axis=1)
-	glm_sim.fit(X, y)
-	score = glm_sim[-1].score(X, y)
-	
-	assert_equal(score.shape[0], 1)
+    glm_sim = GLM(distr='binomial', score_metric='accuracy')
+    X = np.random.randn(n_samples, n_features)
+    y = glm_sim.simulate(beta0, beta, X)
+    y = np.argmax(y, axis=1)
+    glm_sim.fit(X, y)
+    score = glm_sim[-1].score(X, y)
+
+    assert_equal(score.shape[0], 1)
