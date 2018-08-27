d8804bbf73206359c7159424c03c6177d909335d
Pavan Ramkumar <pavan.ramkumar@gmail.com>
2018-08-26 20:12:51 -0700

    docstrings for metrics

--- pyglmnet/metrics.py
+++ pyglmnet/metrics.py
@@ -4,8 +4,22 @@ import numpy as np
 from .pyglmnet import _logL
 
 
-def deviance(y, yhat, ynull_, distr):
-    """Deviance metrics."""
+def deviance(y, yhat, distr):
+    """Deviance metrics.
+
+    Parameters
+    ----------
+    y : array
+        Target labels of shape (n_samples, )
+
+    yhat : array
+        Predicted labels of shape (n_samples, )
+
+    Returns
+    -------
+    score : float
+        Deviance of the predicted labels.
+    """
     if distr in ['softplus', 'poisson']:
         LS = _logL(distr, y, y)
     else:
@@ -17,7 +31,24 @@ def deviance(y, yhat, ynull_, distr):
 
 
 def pseudo_R2(X, y, yhat, ynull_, distr):
-    """Pseudo r2."""
+    """Pseudo-R2 metric.
+
+    Parameters
+    ----------
+    y : array
+        Target labels of shape (n_samples, )
+
+    yhat : array
+        Predicted labels of shape (n_samples, )
+
+    ynull_ : float
+        Mean of the target labels (null model prediction)
+
+    Returns
+    -------
+    score : float
+        Pseudo-R2 score.
+    """
     if distr in ['softplus', 'poisson']:
         LS = _logL(distr, y, y)
     else:
@@ -34,5 +65,19 @@ def pseudo_R2(X, y, yhat, ynull_, distr):
 
 
 def accuracy(y, yhat):
-    """Accuracy."""
+    """Accuracy as ratio of correct predictions.
+
+    Parameters
+    ----------
+    y : array
+        Target labels of shape (n_samples, )
+
+    yhat : array
+        Predicted labels of shape (n_samples, )
+
+    Returns
+    -------
+    accuracy : float
+        Accuracy score.
+    """
     return float(np.sum(y == yhat)) / yhat.shape[0]
