8f7ce78a4f0c143b8c546606e21360d5d306a525
Peter Foley <pwfoley@gmail.com>
2019-01-23 09:18:28 -0800

    don't cache z outside _cdfast

--- pyglmnet/pyglmnet.py
+++ pyglmnet/pyglmnet.py
@@ -615,7 +615,7 @@ class GLM(BaseEstimator):
 
             return result
 
-    def _cdfast(self, X, y, z, ActiveSet, beta, rl):
+    def _cdfast(self, X, y, ActiveSet, beta, rl):
         """
         Perform one cycle of Newton updates for all coordinates.
 
@@ -627,9 +627,6 @@ class GLM(BaseEstimator):
         y : array
             Labels to the data
             n_samples x 1
-        z:  array
-            n_samples x 1
-            beta[0] + X * beta[1:]
         ActiveSet: array
             n_features + 1 x 1
             Active set storing which betas are non-zero
@@ -644,13 +641,11 @@ class GLM(BaseEstimator):
         beta: array
             (n_features + 1) x 1
             Updated parameters
-        z: array
-            beta[0] + X * beta[1:]
-            (n_features + 1) x 1
         """
         n_samples = X.shape[0]
         n_features = X.shape[1]
         reg_scale = rl * (1 - self.alpha)
+        z = beta[0] + np.dot(X, beta[1:])
 
         for k in range(0, n_features + 1):
             # Only update parameters in active set
@@ -677,7 +672,7 @@ class GLM(BaseEstimator):
                 # Update parameters, z
                 update = 1. / hk * gk
                 beta[k], z = beta[k] - update, z - update * xk
-        return beta, z
+        return beta
 
     def fit(self, X, y):
         """The fit function.
@@ -743,7 +738,6 @@ class GLM(BaseEstimator):
 
         if self.solver == 'cdfast':
             ActiveSet = np.ones(n_features + 1)     # init active set
-            z = beta[0] + np.dot(X, beta[1:])       # cache z
 
         # Iterative updates
         for t in range(0, self.max_iter):
@@ -761,8 +755,8 @@ class GLM(BaseEstimator):
 
             elif self.solver == 'cdfast':
                 beta_old = deepcopy(beta)
-                beta, z = \
-                    self._cdfast(X, y, z, ActiveSet, beta, reg_lambda)
+                beta = \
+                    self._cdfast(X, y, ActiveSet, beta, reg_lambda)
                 # Converged if the norm(update) < tol
                 if (t > 1) and (np.linalg.norm(beta - beta_old) < tol):
                     msg = ('\tConverged in {0:d} iterations'.format(t))
