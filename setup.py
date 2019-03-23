e21e175aea2fccbbc6ccd5759fb5afcb9ac3ed13
tommyod <tommy.odland@gmail.com>
2019-03-23 20:33:26 +0100

    getting version without imports

--- setup.py
+++ setup.py
@@ -1,43 +1,55 @@
 #! /usr/bin/env python
 import setuptools  # noqa; we are using a setuptools namespace
-from numpy.distutils.core import setup
-import pyglmnet
+import os
+
+
+def get_version():
+    """
+    Get the version without importing, so as not to invoke dependency
+    requirements.
+    """
+    base, _ = os.path.split(os.path.realpath(__file__))
+    file = os.path.join(base, "pyglmnet", "__init__.py")
+
+    for line in open(file, "r"):
+        if "__version__" in line:
+            return line.split("=")[1].strip().strip("'").strip('"')
+
 
 descr = """Elastic-net regularized generalized linear models."""
 
-DISTNAME = 'pyglmnet'
+DISTNAME = "pyglmnet"
 DESCRIPTION = descr
-MAINTAINER = 'Pavan Ramkumar'
-MAINTAINER_EMAIL = 'pavan.ramkumar@gmail.com'
-LICENSE = 'MIT'
-DOWNLOAD_URL = 'https://github.com/glm-tools/pyglmnet.git'
-VERSION = pyglmnet.__version__
+MAINTAINER = "Pavan Ramkumar"
+MAINTAINER_EMAIL = "pavan.ramkumar@gmail.com"
+LICENSE = "MIT"
+DOWNLOAD_URL = "https://github.com/glm-tools/pyglmnet.git"
+VERSION = get_version()  # Get version without importing
 INSTALL_REQUIRES = ["numpy>=1.11", "scipy>=0.17", "scikit-learn>=0.18", "pytest"]
 
 if __name__ == "__main__":
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          version=VERSION,
-          download_url=DOWNLOAD_URL,
-          install_requires=INSTALL_REQUIRES,
-          long_description=open('README.rst').read(),
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS',
-          ],
-          platforms='any',
-          packages=[
-              'pyglmnet'
-          ],
-          )
+    setuptools.setup(
+        name=DISTNAME,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        install_requires=INSTALL_REQUIRES,
+        long_description=open("README.rst").read(),
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+        ],
+        platforms="any",
+        packages=["pyglmnet"],
+    )
