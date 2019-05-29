90b641cc5898571d22eead64c0f15d075f4e98c8
urwald <scotto@sharpleaf.org>
2019-05-28 19:53:25 -0700

    examples/plot_tikhonov.py
    * use OS-independent temporary files which conflict with other users on the same machine, and also remove them when done

--- examples/plot_tikhonov.py
+++ examples/plot_tikhonov.py
@@ -45,17 +45,19 @@ from pyglmnet import GLMCV
 from spykes.ml.strf import STRF
 
 import matplotlib.pyplot as plt
-
+from tempfile import TemporaryDirectory
+    
 ########################################################
 # Download and fetch data files
 
 from pyglmnet.datasets import fetch_tikhonov_data
-dpath = fetch_tikhonov_data(dpath='/tmp/glm-tools')
 
-fixations_df = pd.read_csv(op.join(dpath, 'fixations.csv'))
-probes_df = pd.read_csv(op.join(dpath, 'probes.csv'))
-probes_df = pd.read_csv(op.join(dpath, 'probes.csv'))
-spikes_df = pd.read_csv(op.join(dpath, 'spiketimes.csv'))
+with TemporaryDirectory(prefix="tmp_glm-tools") as temp_dir: 
+    dpath = fetch_tikhonov_data(dpath=temp_dir)
+    fixations_df = pd.read_csv(op.join(dpath, 'fixations.csv'))
+    probes_df = pd.read_csv(op.join(dpath, 'probes.csv'))
+    probes_df = pd.read_csv(op.join(dpath, 'probes.csv'))
+    spikes_df = pd.read_csv(op.join(dpath, 'spiketimes.csv'))
 
 spiketimes = np.squeeze(spikes_df.values)
 
