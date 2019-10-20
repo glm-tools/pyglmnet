6dab4527feb4c2c36ee95d381e73e7880b9b53d8
Mainak Jas <mainakjas@gmail.com>
2019-10-19 22:53:52 -0400

    FIX: simulate_glm in test_metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,7 +8,7 @@ def test_deviance():
     """Test deviance."""
     n_samples, n_features = 1000, 100
 
-    beta0 = np.random.normal(0.0, 1.0, 1)
+    beta0 = np.random.rand()
     beta = np.random.normal(0.0, 1.0, n_features)
 
     # sample train and test data
@@ -26,7 +26,7 @@ def test_pseudoR2():
     """Test pseudo r2."""
     n_samples, n_features = 1000, 100
 
-    beta0 = np.random.normal(0.0, 1.0, 1)
+    beta0 = np.random.rand()
     beta = np.random.normal(0.0, 1.0, n_features)
 
     # sample train and test data
@@ -44,13 +44,15 @@ def test_accuracy():
     """Testing accuracy."""
     n_samples, n_features, n_classes = 1000, 100, 2
 
-    beta0 = np.random.normal(0.0, 1.0, 1)
-    beta = np.random.normal(0.0, 1.0, (n_features, n_classes))
+    beta0 = np.random.rand()
+    betas = np.random.normal(0.0, 1.0, (n_features, n_classes))
 
     # sample train and test data
     glm_sim = GLM(distr='binomial', score_metric='accuracy')
     X = np.random.randn(n_samples, n_features)
-    y = simulate_glm(glm_sim.distr, beta0, beta, X)
+    y = np.zeros((n_samples, 2))
+    for idx, beta in enumerate(betas.T):
+        y[:, idx] = simulate_glm(glm_sim.distr, beta0, beta, X)
     y = np.argmax(y, axis=1)
     glm_sim.fit(X, y)
     score = glm_sim.score(X, y)
