eeb4c2d0e07011c674426ba994dc1aeee3d38e85
titipata <my.titipat@gmail.com>
2020-02-19 22:43:32 -0500

    Externals from scikit-learn

---
+++ pyglmnet/externals/sklearn/utils/config.py
@@ -0,0 +1,24 @@
+"""Global configuration state and functions for management
+"""
+import os
+
+
+_global_config = {
+    'assume_finite': bool(os.environ.get('SKLEARN_ASSUME_FINITE', False)),
+    'working_memory': int(os.environ.get('SKLEARN_WORKING_MEMORY', 1024)),
+    'print_changed_only': False,
+}
+
+
+def get_config():
+    """Retrieve current values for configuration set by :func:`set_config`
+    Returns
+    -------
+    config : dict
+        Keys are parameter names that can be passed to :func:`set_config`.
+    See Also
+    --------
+    config_context: Context manager for global scikit-learn configuration
+    set_config: Set global scikit-learn configuration
+    """
+    return _global_config.copy()
---
+++ pyglmnet/externals/sklearn/utils/fixes.py
@@ -0,0 +1,32 @@
+"""Compatibility fixes for older version of python, numpy and scipy
+If you add content to this file, please give the version of the package
+at which the fixe is no longer needed.
+"""
+# Authors: Emmanuelle Gouillart <emmanuelle.gouillart@normalesup.org>
+#          Gael Varoquaux <gael.varoquaux@normalesup.org>
+#          Fabian Pedregosa <fpedregosa@acm.org>
+#          Lars Buitinck
+#
+# License: BSD 3 clause
+
+import numpy as np
+
+def _parse_version(version_string):
+    version = []
+    for x in version_string.split('.'):
+        try:
+            version.append(int(x))
+        except ValueError:
+            # x may be of the form dev-1ea1592
+            version.append(x)
+    return tuple(version)
+
+
+np_version = _parse_version(np.__version__)
+
+if np_version < (1, 13):
+    def _object_dtype_isnan(X):
+        return np.frompyfunc(lambda x: x != x, 1, 1)(X).astype(bool)
+else:
+    def _object_dtype_isnan(X):
+        return X != X
