836e6968384da8fc824aa965ff19cc70a7d537f3
titipata <my.titipat@gmail.com>
2020-02-26 14:37:52 -0500

    Fix flake8

--- examples/plot_glm_retinal_ganglia.py
+++ examples/plot_glm_retinal_ganglia.py
@@ -6,19 +6,21 @@ GLM for Spike Trains Prediction in Primate Retinal Ganglion Cells
 
 * Original tutorial adapted from Johnathan Pillow, Princeton University
 * Dataset provided by E.J. Chichilnisky, Stanford University
-* **The dataset is granted by the original authors for the educational use in this tutorial**
-* If you want to use it beyond educational purposes, please contact ``pillow@princeton.edu``
+* The dataset is granted by the original authors for only educational use
+* Please contact ``pillow@princeton.edu`` if using beyond its purposes
 
-First of all, we would like to thank Professor Johnathan Pillow and Professor E.J. Chichilnisky
-for sharing their tutorial dataset. The original MATLAB and Python tutorial can be found from
+First of all, we would like to thank Professor Johnathan Pillow
+and Professor E.J. Chichilnisky for sharing their tutorial dataset.
+The original MATLAB and Python tutorial can be found from
 https://github.com/pillowlab/GLMspiketraintutorial.
 
 These data were collected by Valerie Uzzell in the lab of
 E.J. Chichilnisky at the Salk Institute.  For full information see
 Uzzell et al. (J Neurophys 04) [1]_, or Pillow et al. (J Neurosci 2005) [2]_.
 
-In this tutorial, we will demonstrate how to fit linear GLM and linear-nonlinear GLM 
-(a.k.a Poisson GLM) to predict the spike counts recorded from primate retinal ganglion cells.
+In this tutorial, we will demonstrate how to fit linear GLM and
+linear-nonlinear GLM (a.k.a Poisson GLM) to predict the spike counts
+recorded from primate retinal ganglion cells.
 The dataset contains spike responses from 2 ON and 2 OFF parasol
 retinal ganglion cells (RGCs) in primate retina, stimulated with
 full-field `binary white noise`. Two experiment performed consisted of a
@@ -28,10 +30,12 @@ which can be used for computing the spike-triggered average
 
 References
 ----------
-.. [1] Uzzell, V. J., and E. J. Chichilnisky. "Precision of spike trains in primate
-   retinal ganglion cells." Journal of Neurophysiology 92.2 (2004)
-.. [2] Pillow, Jonathan W., et al. "Prediction and decoding of retinal ganglion cell
-   responses with a probabilistic spiking model." Journal of Neuroscience 25.47 (2005)
+.. [1] Uzzell, V. J., and E. J. Chichilnisky. "Precision of
+   spike trains in primate retinal ganglion cells."
+   Journal of Neurophysiology 92.2 (2004)
+.. [2] Pillow, Jonathan W., et al. "Prediction and decoding of
+   retinal ganglion cell responses with a probabilistic
+   spiking model." Journal of Neuroscience 25.47 (2005)
 
 """
 
@@ -46,7 +50,7 @@ import os.path as op
 import json
 
 import numpy as np
-from numpy.linalg import inv, norm
+from numpy.linalg import inv
 from scipy.linalg import hankel
 
 from pyglmnet import GLM
@@ -56,7 +60,7 @@ from tempfile import TemporaryDirectory
 
 ########################################################
 #
-# Fetch the dataset. The JSON file contains the 
+# Fetch the dataset. The JSON file contains the
 # following keys:  ``stim`` (binary stochastic stimulation),
 # ``stim_times`` (time of the stimulation), and
 # ``spike_times`` (recorded time of the spikes)
@@ -79,24 +83,26 @@ spike_times = [
     np.array(rgcs_dataset['spike_times']['cell_3']),
 ]
 
-n_cells = len(spike_times) # total number of cells
-dt = stim_times[1] - stim_times[0] # time between the stimulation
-n_t = len(stim) # total number of the stimulation
-f = 1. / dt # frequency of the stimulation
+n_cells = len(spike_times)  # total number of cells
+dt = stim_times[1] - stim_times[0]  # time between the stimulation
+n_t = len(stim)  # total number of the stimulation
+f = 1. / dt  # frequency of the stimulation
 
 ########################################################
 #
 # You can pick a cell to work with and visualize the spikes for one second.
 # In this case, we will pick cell number 2 (ON cell).
 
-cell_idx = 2 # pick cell number 2 (ON cell)
-spike_time = spike_times[cell_idx] # pick spike time to work with
-n_spikes = len(spike_time) # number of spikes
+cell_idx = 2  # pick cell number 2 (ON cell)
+spike_time = spike_times[cell_idx]  # pick spike time to work with
+n_spikes = len(spike_time)  # number of spikes
 
 print('Loaded RGC data: cell {}'.format(cell_idx))
-print('Number of stim frames: {:d}  ({:.1f} minutes)'.format(n_t, n_t * dt / 60))
+print('Number of stim frames: {:d} ({:.1f} minutes)'.
+      format(n_t, n_t * dt / 60))
 print('Time bin size: {:.1f} ms'.format(dt * 1000))
-print('Number of spikes: {} (mean rate = {:.1f} Hz)\n'.format(n_spikes, n_spikes / n_t * 60))
+print('Number of spikes: {} (mean rate = {:.1f} Hz)\n'.
+      format(n_spikes, n_spikes / n_t * 60))
 
 # sample indices for visualization
 sample_index = np.arange(120)
@@ -104,13 +110,13 @@ t_sample = dt * sample_index
 
 plt.subplot(2, 1, 1)
 plt.step(t_sample, stim[sample_index])
-plt.title('Raw Stimulus (full field flicker) \
-           and corresponding spike times')
+plt.title('Raw Stimulus (full field flicker)'
+          ' and corresponding spike times')
 plt.ylabel('Stimulation Intensity')
 
 plt.subplot(2, 1, 2)
-tspplot = spike_time[(spike_time >= t_sample.min()) 
-                     & (spike_time < t_sample.max())]
+tspplot = spike_time[(spike_time >= t_sample.min()) &
+                     (spike_time < t_sample.max())]
 plt.stem(spike_time[sample_index],
          [1] * len(spike_time[sample_index]))
 plt.xlim([t_sample.min(), t_sample.max()])
@@ -123,9 +129,9 @@ plt.show()
 #
 # We can use ``scipy``'s function ``hankel`` to make our design matrix.
 
-n_t_filt = 25 # tweak this to see different results
+n_t_filt = 25  # tweak this to see different results
 stim_padded = np.pad(stim, (n_t_filt - 1, 0))
-Xdsgn = hankel(stim_padded[0:-n_t_filt +1], stim[-n_t_filt:])
+Xdsgn = hankel(stim_padded[0: -n_t_filt + 1], stim[-n_t_filt:])
 
 plt.imshow(Xdsgn[:50, :],
            cmap='binary',
@@ -133,8 +139,8 @@ plt.imshow(Xdsgn[:50, :],
            interpolation='nearest')
 plt.xlabel('lags before spike time')
 plt.ylabel('time bin of response')
-plt.title('Sample first 50 rows of design \
-           matrix created using Hankel')
+plt.title('Sample first 50 rows of design'
+          ' matrix created using Hankel')
 plt.colorbar()
 plt.show()
 
@@ -154,7 +160,7 @@ t_bins = np.arange(n_t + 1) * dt
 spikes_binned, _ = np.histogram(spike_time, t_bins)
 sta = Xdsgn.T.dot(spikes_binned) / n_spikes
 
-t_lag = dt * np.arange(-n_t_filt + 1, 1) # time bins for STA (in seconds)
+t_lag = dt * np.arange(-n_t_filt + 1, 1)  # time bins for STA (in seconds)
 plt.plot(t_lag, t_lag * 0, 'k--')
 plt.plot(t_lag, sta, 'bo-')
 plt.title('Spike-Triggered Average (STA)')
@@ -165,16 +171,17 @@ plt.show()
 ########################################################
 # **Fitting and predicting with a linear-Gaussian GLM**
 #
-# For a general linear model, an observed spikes can be thought of an underlying 
-# parameter :math:`\theta` that control the spike output:
+# For a general linear model, an observed spikes can be
+# thought of an underlying parameter
+# :math:`\theta` that control the spike output:
 # :math:`y = \vec{\theta} \cdot \vec{x} + \epsilon` and
 # :math:`y \sim \text{Poiss}(\vec{\theta} \cdot \vec{x})`
 #
 # We can add an offset or a "constant" to our design matrix.
-# This can be done by concatenating a column of 1 to our previously created
-# design matrix.
+# This can be done by concatenating a column of 1 to
+# our previously created design matrix.
 
-Xdsgn_offset = np.hstack((np.ones((n_t,1)), Xdsgn))
+Xdsgn_offset = np.hstack((np.ones((n_t, 1)), Xdsgn))
 
 wsta_offset = inv(Xdsgn_offset.T @ Xdsgn_offset)\
     @ Xdsgn_offset.T.dot(spikes_binned)
@@ -184,7 +191,7 @@ spikes_pred_lgGLM_offset = const + (Xdsgn @ wsta_offset)
 
 ########################################################
 # **Fitting and predicting with a Poisson GLM**
-# 
+#
 # We can also assume that their is a non-linear function governing
 # the underlying the firing patterns. Concreately, we can write down as
 # :math:`y = f(\vec{\theta} \cdot \vec{x}) + \epsilon`, and
@@ -203,7 +210,8 @@ glm_poisson = GLM(distr='poisson',
 glm_poisson.fit(Xdsgn, spikes_binned)
 
 # predict spike counts using Poisson GLM
-# alternatively, you can also use np.exp(glm_poisson.beta0_ + X.dot(glm_poisson.beta_))
+# alternatively, you can also use
+# np.exp(glm_poisson.beta0_ + X.dot(glm_poisson.beta_))
 spikes_pred_poissonGLM = glm_poisson.predict(Xdsgn)
 
 #############################################################################
@@ -214,15 +222,15 @@ spikes_pred_poissonGLM = glm_poisson.predict(Xdsgn)
 # matrix had better be shifted so that we aren't allowed to use the spike
 # count on this time bin to predict itself!
 
-n_t_filt = 25 # same as before, stimulation history
-n_t_hist = 20 # spikes history
+n_t_filt = 25  # same as before, stimulation history
+n_t_hist = 20  # spikes history
 
 # using both stimulation history and spikes history
 spikes_padded = np.pad(spikes_binned, (n_t_hist, 0))
 
-Xstim = hankel(stim_padded[:-n_t_filt +1], stim[-n_t_filt:])
+Xstim = hankel(stim_padded[:-n_t_filt + 1], stim[-n_t_filt:])
 Xspikes = hankel(spikes_padded[:-n_t_hist], stim[-n_t_hist:])
-Xdsgn_hist = np.hstack((Xstim, Xspikes)) # design matrix with spikes history
+Xdsgn_hist = np.hstack((Xstim, Xspikes))  # design matrix with spikes history
 
 # Now, we are ready to fit Poisson GLM with spikes history.
 # create possion GLM instance
@@ -241,7 +249,7 @@ spikes_pred_poissonGLM_hist = glm_poisson_hist.predict(Xdsgn_hist)
 #############################################################################
 # **Putting all together**
 #
-# We are plotting the prediction of spike counts from linear Gaussian GLM, 
+# We are plotting the prediction of spike counts from linear Gaussian GLM,
 # linear Gaussian GLM with offset and poisson prediction for one second.
 
 markerline, _, _ = plt.stem(t_sample, spikes_binned[sample_index])
@@ -251,23 +259,30 @@ plt.plot(t_sample, spikes_pred_lgGLM_offset[sample_index],
 plt.plot(t_sample, spikes_pred_poissonGLM[sample_index],
          color='green', linewidth=2, label='poissonGLM')
 plt.plot(t_sample, spikes_pred_poissonGLM_hist[sample_index],
-         color='orange', linewidth=2, label='poissonGLM_hist')
+         color='red', linewidth=2, label='poissonGLM_hist')
 plt.xlim([t_sample.min(), t_sample.max()])
-plt.title('Spike count prediction using Linear Gaussin \
-           GLM and Poisson GLM predictions')
+plt.title('Spike count prediction')
 plt.xlabel('Time (sec)')
 plt.ylabel('Binned Spike Counts')
 plt.legend()
 plt.show()
 
 # performance of the fitted models
-mse_lgGLM_offset = np.mean((spikes_binned - spikes_pred_lgGLM_offset)**2)  # mean squared error, with offset
-mse_poissonGLM = np.mean((spikes_binned - spikes_pred_poissonGLM)**2) # mean squared error, poissonGLM
-mse_poissonGLM_hist = np.mean((spikes_binned - spikes_pred_poissonGLM_hist)**2) # mean squared error, poissonGLM
-rss = np.mean((spikes_binned - np.mean(spikes_binned))**2) # squared error of spike train
+mse_lgGLM_offset = np.mean((
+    spikes_binned - spikes_pred_lgGLM_offset) ** 2)
+mse_poissonGLM = np.mean((
+    spikes_binned - spikes_pred_poissonGLM) ** 2)
+mse_poissonGLM_hist = np.mean((
+    spikes_binned - spikes_pred_poissonGLM_hist) ** 2)
+rss = np.mean((spikes_binned - np.mean(spikes_binned)) ** 2)
 
-print('Training perf (R^2): lin-gauss GLM, w/ offset: {:.2f}'.format(1 - mse_lgGLM_offset / rss))
-print('Training perf (R^2): poisson GLM {:.2f}'.format(1 - mse_poissonGLM / rss))
-print('Training perf (R^2): poisson GLM w/ spikes history {:.2f}'.format(1 - mse_poissonGLM_hist / rss))
-print('Training perf (R^2): Pyglmnet possion GLM {:.2f}'.format(glm_poisson.score(Xdsgn, spikes_binned)))
-print('Training perf (R^2): Pyglmnet poisson GLM w/ spikes history {:.2f}'.format(glm_poisson_hist.score(Xdsgn_hist, spikes_binned)))
+print('Training perf (R^2): lin-gauss GLM, w/ offset: {:.2f}'
+      .format(1 - mse_lgGLM_offset / rss))
+print('Training perf (R^2): poisson GLM {:.2f}'
+      .format(1 - mse_poissonGLM / rss))
+print('Training perf (R^2): poisson GLM w/ spikes history {:.2f}'
+      .format(1 - mse_poissonGLM_hist / rss))
+print('Training perf (R^2): Pyglmnet possion GLM {:.2f}'
+      .format(glm_poisson.score(Xdsgn, spikes_binned)))
+print('Training perf (R^2): Pyglmnet poisson GLM w/ spikes history {:.2f}'
+      .format(glm_poisson_hist.score(Xdsgn_hist, spikes_binned)))
